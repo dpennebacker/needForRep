6cd100efdee08dfe4cad664cffa04af9f71999a0
Evan Older <evan.older@gmail.com>
2017-09-26 08:46:04 -0400

    Added a prompt since I forget one, whoops.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+    	System.out.println("Enter word to \"encrypt\"");
         Scanner sysin = new Scanner(System.in);
         String word = sysin.nextLine();
 	    int perc = (int)(Math.random() * word.length());
